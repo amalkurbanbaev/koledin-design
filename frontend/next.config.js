376077814cf66c6fd7227cbf2be59c94cc9b3c65
RarogCmex <denis7774@gmail.com>
2023-06-29 21:46:07 +0500

    Updated frontend dockerfile to working state

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,13 +3,14 @@ const nextConfig = {
     images: {
         remotePatterns: [
             {
-                protocol: "http",
-                hostname: "127.0.0.1",
-                port: "1337",
+                protocol: "https",
+                hostname: "admin.koledin.com",
+                port: "443",
                 pathname: "/uploads/**",
             },
         ],
     },
+    output: 'standalone',
 };
 
 module.exports = nextConfig;
