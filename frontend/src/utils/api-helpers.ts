313f85849d45bff5d147ad5e5fc4b0a03acf28a0
amalkurbanbaev <amalkurbanbaev@yandex.ru>
2023-07-04 01:40:37 +0300

    fix: product url rollback

--- frontend/src/utils/api-helpers.ts
+++ frontend/src/utils/api-helpers.ts
@@ -1,8 +1,9 @@
-export const API_URL = process.env.NEXT_PUBLIC_STRAPI_API_URL;
+export const API_URL =
+    process.env.NEXT_PUBLIC_STRAPI_API_URL || "http://127.0.0.1:1337";
 
 export function getStrapiURL(path = "") {
     const baseURL =
-        process.env.NODE_ENV !== "development"
+        process.env.NODE_ENV === "development"
             ? API_URL
             : "http://127.0.0.1:1337"; // TODO
 
