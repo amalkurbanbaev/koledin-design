f6e1dac404a4554224af46b3f2902c77eb5df210
RarogCmex <denis7774@gmail.com>
2023-07-05 23:44:05 +0500

    Revert, it will not fix :)

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  url: env('PUBLIC_URL', 'https://admin.koledin.com/api'),
+  url: env('PUBLIC_URL', 'https://admin.koledin.com'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
